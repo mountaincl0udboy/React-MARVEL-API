3ce23aa4169ee8bc08041ba04534b6ed5e27d281
ogkkk <takemefire.99@mail.ru>
2022-11-23 01:35:31 +0600

    App,Index JS updates

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Nav from "./components/Nav/Nav";
+import Home from "./components/Home/Home";
+import CharacterList from "./components/CharacterList/CharacterList";
+import ComicsList from "./components/ComicsList/ComicsList";
+import EventsList from "./components/EventsList/EventsList";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Page404 from "./components/Page404/Page404";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/characters" element={<CharacterList/>}/>
+          <Route path="/comics" element={<ComicsList />} />
+          <Route path="/events" element={<EventsList />} />
+          <Route path="/*" element={<Page404 />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
